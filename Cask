131eb6826c65843e2584d28392c0f520615ab5d9
Jeff <Bellegarde@Jeffs-MBP.home>
2016-02-03 22:00:02 -0800

    Make it a proper package.
    Still no tests.

---
+++ Cask
@@ -0,0 +1,10 @@
+(source gnu)
+(source melpa)
+
+(package-file "region-command-mode.el")
+
+(development
+  (depends-on "flycheck-cask")
+  (depends-on "undercover")
+  (depends-on "el-mock")
+  (depends-on "ert-runner"))
--- region-command-mode.el
+++ region-command-mode.el
@@ -1,3 +1,25 @@
+;;; region-command-mode.el --- Emacs to activate keybindings when region is active. -*- lexical-binding: t -*-
+;; Copyright (C) 2016  Jeff Bellegarde
+
+;; Author: Jeff Bellegarde <bellegar@gmail.com>
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
 (require 'bind-key)
 
 (make-variable-buffer-local
@@ -70,3 +92,5 @@ hook `region-command-active-mode-hook' can be used."
 
 
 (provide 'region-command-mode)
+
+;;; region-command-mode.el ends here
