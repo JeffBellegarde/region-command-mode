626a8b6818c99779c4cb722879f7bae75e66a448
Jeff <Bellegarde@Jeffs-MBP.home>
2016-02-03 21:46:12 -0800

    Deactive in post-command-hook.
    This prevents the the mode being disabled multiple times while
    processing a command.

--- region-command-mode.el
+++ region-command-mode.el
@@ -5,7 +5,7 @@
    "The original position of point when region-command-mode was started."))
 
 (make-variable-buffer-local
- (defvar region-command-mode-active nil))
+ (defvar region-command-mode-depth 0))
 
 (defvar region-command-mode-keymap (make-sparse-keymap))
 
@@ -16,20 +16,12 @@
   (deactivate-mark))
 ;; (setq deactivate-mark-hook '())
 (bind-keys :map region-command-mode-keymap
-           ;; ("c" .  (lambda ()
-           ;;           (interactive)
-           ;;           (message "c")
-           ;;           (deactivate-mark)))
            ("q" . region-command-mode-quit)
            ("<SPC>" . region-command-mode-deactivate)
            ("r" . rectangle-mark-mode)
-           ;; ("C-<SPC>" . (lambda ()
-           ;;                (interactive)
-           ;;                (set-mark-command nil)
-           ;;                (set-mark-command (point))))
            ("x" . exchange-point-and-mark))
 
-(define-minor-mode region-command-mode-active-mode
+(define-minor-mode region-command-active-mode
   "Allows commands when the region is active.
 
 This the internal mode that is activated when the region is active. It should
@@ -39,31 +31,42 @@ not be used directly. When active, an additional keymap
 hook `region-command-active-mode-hook' can be used."
   :lighter " RCA"
   :keymap region-command-mode-keymap
-  (if region-command-mode-active-mode
+  (if region-command-active-mode
       (progn
+        (add-hook 'post-command-hook 'region-command-mode--check-if-done)
         (setq region-command-mode-start-position (point)))
+    (remove-hook 'post-command-hook 'region-command-mode--check-if-done)
     (setq region-command-mode-start-position nil)))
 
+(defun region-command-mode--check-if-done ()
+  ;; (message "checking if done %s %s" mark-active deactivate-mark)
+  (when (or (not mark-active) deactivate-mark)
+    (region-command-mode-deactivate)))
+
 (defun region-command-mode-activate ()
-  (region-command-mode-active-mode 1))
+  (unless region-command-active-mode
+    ;; (message "region-command-mode-activating")
+    (region-command-active-mode 1)))
 
 (defun region-command-mode-deactivate ()
   (interactive)
-  (region-command-mode-active-mode 0))
+  ;; (message "region-command-mode-deactivating")
+  (region-command-active-mode 0))
 
+;;;###autoload
 (define-minor-mode region-command-mode
   "Enable region command mode.
-
-
-
 "
   :lighter " RC"
   :global t
   (if region-command-mode
       (progn
         (add-hook 'activate-mark-hook 'region-command-mode-activate)
-        (add-hook 'deactivate-mark-hook 'region-command-mode-deactivate))
-    (add-hook 'activate-mark-hook 'region-command-mode-activate)
-    (add-hook 'deactivate-mark-hook 'region-command-mode-deactivate)
+        ;; (add-hook 'deactivate-mark-hook 'region-command-mode-deactivate)
+        )
+    (remove-hook 'activate-mark-hook 'region-command-mode-activate)
+    ;; (remove-hook 'deactivate-mark-hook 'region-command-mode-deactivate)
     (region-command-mode-deactivate)))
+
+
 (provide 'region-command-mode)
